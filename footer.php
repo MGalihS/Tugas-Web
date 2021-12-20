d1e5efcbd0a1007782508f270d93595d5b1f57f0
Magi112 <58874932+Magi112@users.noreply.github.com>
2021-12-20 19:13:21 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<!-- Footer -->
-<footer class="bg-primary text-white text-center">
-    <P>Created By Muhamad Galih Sundayana Ditujukan untuk memenuhi Tugas Pengembangan Aplikasi Berbasis Web</P>
-</footer>
-<!-- Akhir Footer -->
